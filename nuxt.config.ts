a32921a93b0f0345e7400df905b2fcccf3fb05b8
Abraão Reis <andradeabraao23@gmail.com>
2025-05-30 23:34:15 -0300

    build for activity

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
+  ssr: true,
+  nitro: {
+    preset: 'static'
+  },
   devtools: { enabled: true },
   pages: true,
   app: {
--- pages/projects.vue
+++ pages/projects.vue
@@ -14,54 +14,69 @@ const projectsInfos = [
     {
         title: 'Portfólio Profissional 1.0',
         description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis, repudiandae.',
-        urlImg: images['../assets/images/project-portfolio1.jpg']
+        urlImg: images['../assets/images/project-portfolio1.jpg'],
+        link: 'https://abraao-reyys.github.io/portfolio-profissional/'
     },
     {
         title: 'Blog Android',
         description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis, repudiandae.',
-        urlImg: images['../assets/images/project-blogandroid.jpg']
+        urlImg: images['../assets/images/project-blogandroid.jpg'],
+        link: 'https://abraao-reyys.github.io/projeto-android/'
     },
     {
         title: 'Tela de Login',
         description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis, repudiandae.',
-        urlImg: images['../assets/images/project-teladelogin.jpg']
+        urlImg: images['../assets/images/project-teladelogin.jpg'],
+        link: 'https://abraao-reyys.github.io/projeto-login/'
     },
     {
         title: 'Landing Page - One Piece',
         description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis, repudiandae.',
-        urlImg: images['../assets/images/project-onepiece.jpg']
+        urlImg: images['../assets/images/project-onepiece.jpg'],
+        link: 'https://abraao-reyys.github.io/projeto-one-piece/'
     },
     {
         title: 'Cordel Moderno',
         description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis, repudiandae.',
-        urlImg: images['../assets/images/project-cordel.jpg']
+        urlImg: images['../assets/images/project-cordel.jpg'],
+        link: 'https://abraao-reyys.github.io/projeto-cordel/'
     },
     {
         title: 'Simulador de Smartphone',
         description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis, repudiandae.',
-        urlImg: images['../assets/images/project-smartphone.jpg']
+        urlImg: images['../assets/images/project-smartphone.jpg'],
+        link: 'https://abraao-reyys.github.io/projeto-social-media/'
     },
     {
         title: 'Landing Page - GameZone',
         description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis, repudiandae.',
-        urlImg: images['../assets/images/project-gamezone.jpg']
+        urlImg: images['../assets/images/project-gamezone.jpg'],
+        link: 'https://marcos-grocha.github.io/game-zone'
     }
 ];
 </script>
 
 <template>
-    <main>
-        <h2>Galeria de Projetos</h2>
-        <div class="project-cards-container">
-            <ProjectCard v-for="project in projectsInfos" :urlImage="project.urlImg">
-                <template #projectTitle>{{ project.title }}</template>
-                <template #projectDescription>
-                    {{ project.description }}
-                </template>
-            </ProjectCard>
-        </div>
-    </main>
+  <main>
+    <h2>Galeria de Projetos</h2>
+    <div class="project-cards-container">
+      <a
+        v-for="project in projectsInfos"
+        :key="project.title"
+        :href="project.link"
+        class="project-link-wrapper"
+        target="_blank"
+        rel="noopener"
+      >
+        <ProjectCard :urlImage="project.urlImg">
+          <template #projectTitle>{{ project.title }}</template>
+          <template #projectDescription>{{ project.description }}</template>
+        </ProjectCard>
+      </a>
+    </div>
+  </main>
 </template>
+
 
 <style scoped>
 main {
@@ -84,7 +99,13 @@ main {
         height: 100%;
         max-width: 1256px;
         width: 100%;
+
+        cursor: pointer;
     }
+}
+
+a {
+    text-decoration: none;
 }
 
 @media screen and (min-width: 360px) and (max-width: 500px) {
